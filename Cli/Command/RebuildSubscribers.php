621f4a26f3f8295da1b8c8fb8f51e51f2bf42d0c
Simon Hampel <simon@hampelgroup.com>
2025-09-12 17:23:47 +1000

    rebuild subscribers CLI command

---
+++ Cli/Command/RebuildSubscribers.php
@@ -0,0 +1,32 @@
+<?php namespace Hampel\Newsletters\Cli\Command;
+
+use Hampel\Newsletters\Job\SubscriberRebuild;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use XF\Cli\Command\JobRunnerTrait;
+
+class RebuildSubscribers extends Command
+{
+    use JobRunnerTrait;
+
+	protected function configure()
+	{
+		$this
+			->setName('newsletters:rebuild-subscribers')
+			->setDescription('Rebuild subscriber data from currently registered users');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->setupAndRunJob(
+            'newslettersSubscriberRebuild',
+            SubscriberRebuild::class,
+            [],
+            $output
+        );
+
+        return Command::SUCCESS;
+    }
+}
